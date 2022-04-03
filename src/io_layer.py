f07b028ed9fbdb2a6ee04f945f51e3a90a65cd60
Aaron Hillegass <gtg129u@gatech.edu>
2022-04-02 22:33:48 -0400

    Added mating

--- src/io_layer.py
+++ src/io_layer.py
@@ -1,15 +1,24 @@
 import json
+import random
 
 # This is designed to create an abstraction for reading and writing 
-# the candidates and their stats
+# the candidates and their stats so I can move it to a database or 
+# FTP server.
+class io_layer:
+    def fetch_all_config(self):
+        with open("../initial_configs/_all.json") as f:
+            result = json.load(f)
+        return result
 
-def fetch_all_config():
-    with open("../initial_configs/_all.json") as f:
-        result = json.load(f)
-    return result
+    def fetch_config(self, config_name):
+        with open(f"../initial_configs/{config_name}.conf") as f:
+            result = json.load(f)
+        return result
 
-def fetch_config(config_name):
-    with open(f"../initial_configs/{config_name}.conf") as f:
-        result = json.load(f)
-    return result
+    def store_config(self,config):
+        with open(f"../initial_configs/{config['name']}.conf", "w") as f:
+            json.dump(config, f, indent=2)
 
+    def new_name(self):
+        v = random.randint(0, 1000000000)
+        return f"{v:08X}"
