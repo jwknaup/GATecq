73949b6a2cf31037f56bb70210e63f4438af663a
Aaron Hillegass <gtg129u@gatech.edu>
2022-04-20 15:37:02 -0400

    Forgot to clear hidden state of qnet

--- src/Harness.py
+++ src/Harness.py
@@ -18,14 +18,22 @@ class Harness:
         self.loss_fn = torch.nn.MSELoss()
     
     def start_new_rollout(self):
+        # Make lists to hold the data from this rollout
         self.current_state_list = []
         self.current_action_list = []
         self.current_reward_list = []
+        # Clear the memory of the qnet
+        self.qnet.reset_hidden()
 
     def action_for_state(self, state, eval_mode=False):
+        # Don't learn here
         with torch.no_grad():
             all_utilities = self.qnet(state)
+
+        # This is what our network thinks is the best action
         action = torch.argmax(all_utilities)
+
+        # If we are learning, then we need to explore
         if not eval_mode:
             # Epsilon-greedy
             if random.random() < self.epsilon:
@@ -35,15 +43,21 @@ class Harness:
             self.current_action_list.append(action)
         return action
 
+    # Note the reward experiencd after the last action
     def set_reward_for_last_action(self, reward):
         self.current_reward_list.append(reward)
 
     def end_rollout(self):
+        # Move the data for the current rollout into the replay buffer
         self.replay_buffer_states.append(self.current_state_list)
         self.replay_buffer_actions.append(self.current_action_list)
+
+        # Utility is the discounted future rewards
         utility_list = []
         current_utility = 0.0
+        # Walk the list of rewards backwards
         for rr in reversed(self.current_reward_list):
+            # Discount future rewards
             current_utility = rr + self.discount_factor * current_utility
             utility_list.insert(0, current_utility)
         self.replay_buffer_utility.append(utility_list)
@@ -52,14 +66,20 @@ class Harness:
             self.replay_buffer_states.pop(0)
             self.replay_buffer_actions.pop(0)
             self.replay_buffer_utility.pop(0)
-        
+    
     def learn_from_replay_buffer(self):
         rollout_count = len(self.replay_buffer_actions)
         for i in range(self.learning_iterations):
+            # Pick a random rollout
             current_rollout = random.randint(0, rollout_count - 1)
             current_state_list = self.replay_buffer_states[current_rollout]
             current_action_list = self.replay_buffer_actions[current_rollout]
             current_utility_list = self.replay_buffer_utility[current_rollout]
+
+            # Clear the memory of the qnet
+            self.qnet.reset_hidden()
+
+            # Walk forward throught the states
             for j in range(len(current_state_list)):
                 state = current_state_list[j]
                 action = current_action_list[j]
--- src/test_harness.py
+++ src/test_harness.py
@@ -5,30 +5,33 @@ import numpy as np
 import torch
 import logging
 
+# See log messages
 logging.basicConfig(level=logging.DEBUG)
 
 io = io_layer.io_layer()
-
 all_conf = io.fetch_all_config()
 
 # Two input test vectors
 test_data1 = torch.Tensor(np.random.rand(1, all_conf["other_inputs"] + all_conf["lidar_inputs"]))
 test_data2 = torch.Tensor(np.random.rand(1, all_conf["other_inputs"] + all_conf["lidar_inputs"]))
-
-test_conf0 = io.fetch_config("0")
-test_harness = Harness.Harness(test_conf0, all_conf)
-
 in_data = [test_data1, test_data2]
 
+# Fetch a configuration
+test_conf0 = io.fetch_config("0")
+
+# Create a harness for Deep Q learning
+test_harness = Harness.Harness(test_conf0, all_conf)
+
+# Do a rollout
 test_harness.start_new_rollout()
 action = test_harness.action_for_state(in_data[0])
 print(f"Action 0: {action}")
-test_harness.set_reward_for_last_action(0.0)
-
+test_harness.set_reward_for_last_action(-1.0)
 action = test_harness.action_for_state(in_data[1])
 print(f"Action 1: {action}")
 test_harness.set_reward_for_last_action(5.0)
-
 test_harness.end_rollout()
+
+# Learn from the replay buffer
 test_harness.learn_from_replay_buffer()
 
